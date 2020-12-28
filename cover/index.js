c5de63bf3d8911e1f3fffa71839a4bf74f830e17
IMHOJEONG <39ghwjd@naver.com>
2020-12-28 15:01:29 +0900

    12.26~12.27 홍보 기본 구조, 파일 추가

---
+++ cover/index.js
@@ -0,0 +1,9 @@
+(function(){
+    let featuresButton = document.getElementsByClassName("features");
+    let contactButton = document.getElementsByClassName("contact");
+
+    // featuresButton.addEventListener('click',{
+    //     // let homeButton = document.getElementsByClassName("home")
+    // });
+
+})();
